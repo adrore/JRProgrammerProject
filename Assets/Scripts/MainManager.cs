8061cc397905daa17d9a7117cb013dc763cc3fc7
Adrián <107645389+adrore@users.noreply.github.com>
2022-06-17 13:56:16 +0200

    persistance
    persistance between sceens and sesions

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class MainManager : MonoBehaviour
+{
+    // Start() and Update() methods deleted - we don't need them right now
+
+    public static MainManager instance;
+    public Color teamColor;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+        LoadColor();
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public Color teamColor;
+    }
+
+    public void SaveColor()
+    {
+        SaveData data = new SaveData();
+        data.teamColor = teamColor;
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+            teamColor = data.teamColor;
+        }
+    }
+}
